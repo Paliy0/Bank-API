4e19e6969ee104c8ba8990724cfb93f2d89d408c
Sasha <646415@student.inholland.nl>
2023-06-12 18:56:05 +0200

    fixed add accounts

--- src/main/java/nl/inholland/Bank/API/service/AccountService.java
+++ src/main/java/nl/inholland/Bank/API/service/AccountService.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
