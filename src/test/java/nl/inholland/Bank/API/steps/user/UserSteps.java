19469f0678c3d6ed1f739856a69ae8848574e06f
jojunicorn <684123@student.inholland.nl>
2023-06-29 07:59:36 +0200

    junit testing user

--- src/test/java/nl/inholland/Bank/API/steps/user/UserSteps.java
+++ src/test/java/nl/inholland/Bank/API/steps/user/UserSteps.java
@@ -25,78 +25,69 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class UserSteps {
-
-//    private UserService userService;
-//    private User registeredUser;
-//    private UserRequestDTO userDetails;
-//    private List<UserResponseDTO> allUsers;
-//    private int updatedDailyLimit;
-//    private int updatedTransactionLimit;
-//    private Response serverResponse;
-//
 //    @Given("the user has logged in as a normal user")
 //    public void userLoggedInAsNormalUser() {
 //        // Implement the necessary steps to log in as a normal user
 //    }
 //
-//    @When("the client requests to get all users")
-//    public void clientRequestsGetAllUsers() {
-//        allUsers = userService.getAllUsers(true, 0,50);
+//    @Given("the user provides valid user details")
+//    public void userProvidesValidUserDetails() {
+//        // Implement the necessary steps for the user to provide valid user details
+//    }
+//
+//    @When("the client requests to register the user")
+//    public void clientRequestsToRegisterUser() {
+//        // Implement the steps to make a request to register the user
+//    }
+//
+//    @Then("the server should respond with status code {int}")
+//    public void serverRespondsWithStatusCode(int statusCode) {
+//        // Implement the steps to assert the server's response status code
+//    }
+//
+//    @Given("the user is logged in and provides data to be changed")
+//    public void userLoggedInAndProvidesDataToBeChanged() {
+//        // Implement the necessary steps for the user to be logged in and provide data to be changed
+//    }
+//
+//    @When("the user requests to change the data")
+//    public void userRequestsToChangeData() {
+//        // Implement the steps to make a request to change the user's data
+//    }
+//
+//    @Given("the user is logged in and wants to get the transaction limit")
+//    public void userLoggedInAndWantsToGetTransactionLimit() {
+//        // Implement the necessary steps for the user to be logged in and want to get the transaction limit
+//    }
+//
+//    @When("the client requests to get the transaction limit")
+//    public void clientRequestsToGetTransactionLimit() {
+//        // Implement the steps to make a request to get the transaction limit
 //    }
 //
 //    @Then("the server should respond with status code 200")
 //    public void serverRespondsWithStatusCode200() {
-//        assertEquals(200, serverResponse.getStatus());
+//        // Implement the steps to assert the server's response with status code 200
 //    }
 //
-//    @And("the response body should contain a list of users")
-//    public void responseBodyContainsListOfUsers() {
-//        assertTrue(allUsers.size() > 0);
+//    @And("the server responds with the transaction limit")
+//    public void serverRespondsWithTransactionLimit() {
+//        // Implement the steps to assert the server's response with the transaction limit
 //    }
 //
-//    @When("the user provides valid user details")
-//    public void userProvidesValidUserDetails() {
-//        userDetails = new UserRequestDTO("Test", "User", "test@user.com", "Test123!", "+4912345678", "12345678", "2000-10-10", "Mainstreet", 4, "1044CD", "Haarlem", "Netherlands");
+//    @Given("the user is logged in and wants to get the daily limit")
+//    public void userLoggedInAndWantsToGetDailyLimit() {
+//        // Implement the necessary steps for the user to be logged in and want to get the daily limit
 //    }
 //
-//    @When("the client requests to register the user")
-//    public void clientRequestsRegisterUser() {
-//        Boolean registered = userService.registerLogic(userDetails);
+//    @When("the client requests to get the daily limit")
+//    public void clientRequestsToGetDailyLimit() {
+//        // Implement the steps to make a request to get the daily limit
 //    }
 //
-//    @Then("the response body should contain a success message")
-//    public void responseBodyContainsSuccessMessage() {
-//        String responseBody = serverResponse.getMessage();
-//        assertTrue(responseBody.contains("success"));
+//    @And("the server responds with the daily limit")
+//    public void serverRespondsWithDailyLimit() {
+//        // Implement the steps to assert the server's response with the daily limit
 //    }
-//
-//    @When("the client requests to delete a user by ID")
-//    public void clientRequestsDeleteUserByID() {
-//        userService.deleteUserOrDeactivate(userId);
-//    }
-//
-//    @When("the client requests to update the user's daily limit")
-//    public void clientRequestsUpdateUserDailyLimit() {
-//        updatedDailyLimit = userService.updateUserDailyLimit(userId, newDailyLimit);
-//    }
-//
-//    @Then("the response body should contain the updated daily limit")
-//    public void responseBodyContainsUpdatedDailyLimit() {
-//        int responseBodyLimit = Integer.parseInt(serverResponse.getBody());
-//        assertEquals(updatedDailyLimit, responseBodyLimit);
-//    }
-//
-//    @When("the client requests to update the user's transaction limit")
-//    public void clientRequestsUpdateUserTransactionLimit() {
-//        updatedTransactionLimit = userService.updateUserTransactionLimit(userId, newTransactionLimit);
-//    }
-//
-//    @Then("the response body should contain the updated transaction limit")
-//    public void responseBodyContainsUpdatedTransactionLimit() {
-//        int responseBodyLimit = Integer.parseInt(serverResponse.getBody());
-//        assertEquals(updatedTransactionLimit, responseBodyLimit);
-//    }
-//
-
 
 }
--- src/test/java/resources/features/user.feature
+++ src/test/java/resources/features/user.feature
@@ -1,32 +1,24 @@
-Feature: User Management
-
-  Background:
-    Given the user has logged in as a normal user
-
-  Scenario: Get all users
-    When the client requests to get all users
-    Then the server should respond with status code 200
-    And the response body should contain a list of users
-
-  Scenario: Register a user (Normal User)
-    Given the user provides valid user details
-    When the client requests to register the user
-    Then the server should respond with status code 200
-    And the response body should contain a success message
-
-  Scenario: Delete a user (Employee Only)
-    When the client requests to delete a user by ID
-    Then the server should respond with status code 200
-    And the response body should contain a success message
-
-  Scenario: Update a user's daily limit (Employee Only)
-    And the employee provides a new daily limit
-    When the client requests to update the user's daily limit
-    Then the server should respond with status code 200
-    And the response body should contain the updated daily limit
-
-  Scenario: Update a user's transaction limit (Employee Only)
-    And the employee provides a new transaction limit
-    When the client requests to update the user's transaction limit
-    Then the server should respond with status code 200
-    And the response body should contain the updated transaction limit
+#Feature: User Management
+#
+#  Background:
+#    Given the user has logged in as a normal user
+#
+#  Scenario: Register a user
+#    Given the user provides valid user details
+#    When the client requests to register the user
+#    Then the server should respond with status code 200
+#
+#  Scenario: Change user details
+#    Given the user is logged in and provides data to be changed
+#    When the user requests to change the data
+#    Then the server should respond with status code 200
+#
+#  Scenario: Get transaction limit
+#    When the client requests to get the transaction limit
+#    Then the server should respond with status code 200
+#    And the server responds with the transaction limit
+#
+#  Scenario: Get daily limit
+#    When the client requests to get the daily limit
+#    Then the server should respond with status code 200
+#    And the server responds with the daily limit
